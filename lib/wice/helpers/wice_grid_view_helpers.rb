8005743cb0a1b5917826911b22a944a15c19c2a9
Galih Akbar Moerbayaksa <moer@student.uns.ac.id>
2020-02-02 12:35:07 +0700

    auah

--- lib/wice/helpers/wice_grid_view_helpers.rb
+++ lib/wice/helpers/wice_grid_view_helpers.rb
@@ -629,12 +629,10 @@ module Wice
       )
 
       if content.nil? || content.empty?
-        content << '<ul class="list-inline mb-0"></ul>'
+        content << ('<ul class="list-inline mb-0"></ul>').html_safe
       end
 
-      content << '<ul class="list-inline list-inline-dotted mb-0 mt-2 mt-sm-0">' + html + '</ul>'
-
-      (content).html_safe
+      content << ('<ul class="list-inline list-inline-dotted mb-0 mt-2 mt-sm-0">' + html + '</ul>').html_safe
     end
 
     def show_all_link(collection_total_entries, parameters, _grid_name) #:nodoc:
